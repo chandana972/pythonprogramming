5b265204ab43012b307bd4b5e691fcfd4f44c4d2
chandana972 <42571801+chandana972@users.noreply.github.com>
2018-09-02 22:00:04 +0530

    Create consonantvowel.py

---
+++ consonantvowel.py
@@ -0,0 +1,9 @@
+x=str(input(""))
+if x.isalpha()==True:
+  if(x=='a'or x=='A' or x=='e' or x=='E' or x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U'):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
+
