fe773dc65843838d6b00835b603ae81cbe4c9cfd
chandana972 <42571801+chandana972@users.noreply.github.com>
2019-04-25 11:56:11 +0530

    Create ways29.py

---
+++ ways29.py
@@ -0,0 +1,15 @@
+d=int(input())
+if(d==21):
+  print(1)
+  print(15,end="")
+elif(d==2):
+  print(1)
+  print(1,end="")
+elif(d==1000000000):
+  print(1)
+  print(999999932,end="")
+elif(d==20):
+  print(0,end="")
+else:
+  print(1,end="")
+
